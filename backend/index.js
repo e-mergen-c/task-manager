e6e9836b125f30ce883520641e1c7bdd63a9878f
dattes git <mergen.ganbaatar@gmail.com>
2023-02-16 23:22:37 -0600

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,10 @@ app.use('/api', allRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+        res.sendFile(path.resolve(__dirname, "dist", "index.html"))
     );
     } else {
     app.get("/", (req, res) => {
